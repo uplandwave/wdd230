72b8b673eb2a811c2faa53420741605bebf1a75f
uplandwave <113386040+uplandwave@users.noreply.github.com>
2024-01-19 16:02:38 -0700

    new site added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,16 @@
 // document.lastModified;
 // returns: Tuesday, December 16, 2017 11:09:42
 
-// Dynamically populate the current year in the footer's first paragraph
-document.getElementById('currentYear').innerText = new Date().getFullYear();
+//  Dynamically populate the current year in the footer's first paragraph
+// document.getElementById('currentYear').innerText = new Date().getFullYear();
 
 // Dynamically populate the date the document was last modified in the second paragraph
-document.getElementById('lastModified').innerText = "Last modified: " + document.lastModified;
-document.getElementById("lastModified").innerText = "Last modified: " + lastModifiedTimestamp;
+// document.getElementById('lastModified').innerText = "Last modified: " + document.lastModified;
+// document.getElementById("lastModified").innerText = "Last modified: " + lastModifiedTimestamp;
+
+
+let currentYear = new Date().getFullYear();
+document.getElementById("copyYear").textContent = currentYear;
+
+let lastMod = new Date(document.lastModified);
+document.getElementById("lastModified").textContent = `Last Updated: ${lastMod}`;
