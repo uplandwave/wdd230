29d826e905c9c99ac87bdc4b4290862f48149619
uplandwave <113386040+uplandwave@users.noreply.github.com>
2024-03-19 17:47:32 -0600

    All Navs work

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -79,4 +79,4 @@ fetch(
 function calculateWindChill(temperature, windSpeed) {
     const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
     return Math.round(windChill);
-}
+} 
