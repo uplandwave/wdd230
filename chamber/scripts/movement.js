15e68fc0506251d02ac55f6e96c80c9ee9757791
uplandwave <113386040+uplandwave@users.noreply.github.com>
2024-03-20 18:25:33 -0600

    whole new page lots of pictures

--- chamber/scripts/movement.js
+++ chamber/scripts/movement.js
@@ -41,4 +41,9 @@ function goToFullscreen(windowId) {
 function toggleDarkMode() {
     var desktop = document.getElementById('desktop');
     desktop.classList.toggle('dark-mode');
+    var desktop1 = document.getElementsByClassName('window');
+    desktop1.classList.toggle('dark-mode-window');
+    var desktop2 = document.getElementById('taskbar');
+    desktop2.classList.toggle('dark-mode-taskbar');
+
 }
